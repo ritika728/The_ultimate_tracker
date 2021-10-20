862bbc8bfdae280e8d2563cdcc11c1ccad32907e
Deepali Singal <72861787+deepali2002-max@users.noreply.github.com>
2021-10-20 22:44:25 +0530

    Add files via upload

---
+++ alert.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import json
+import urllib
+import urllib.request
+import smtplib
+from email.message import EmailMessage
+def funcalert():
+    url = "https://eonet.sci.gsfc.nasa.gov/api/v2.1/events"
+    response = urllib.request.urlopen(url)
+    text = response.read()
+    json_data = json.loads(text)
+    df = pd.json_normalize(json_data['events'])
+    file = open('city.txt','r')
+    contents = file.readlines()
+    for i in contents:
+        for j in range(0,len(df)):
+            if(i==(df.title[j]+"\n")):
+                print("hello")
+                gmail_user = 'paglol1999@gmail.com'
+                gmail_password = 'paglol14'
+
+                sent_from = gmail_user
+                to = ['lambaankita24@gmail.com', 'deepalisingal2002@gmail.com']
+                subject = 'Lorem ipsum dolor sit amet'
+                body = 'consectetur adipiscing elit'
+
+                email_text = """\
+                From: %s
+                To: %s
+                Subject: %s
+
+                %s
+                """ % (sent_from, ", ".join(to), subject, body)
+
+                try:
+                    smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+                    smtp_server.ehlo()
+                    smtp_server.login(gmail_user, gmail_password)
+                    smtp_server.sendmail(sent_from, to, email_text)
+                    smtp_server.close()
+                    print ("Email sent successfully!")
+                except Exception as ex:
+                    print ("Something went wrong….",ex)
+                break
+funcalert()
